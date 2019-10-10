71d0a4b770970ec5b99d0ee78e9b929043655567
takayuki_kashiwakura <kashiwakura_takayuki@rubygroupe.jp>
2019-10-10 12:46:22 +0900

    コメント取得API作成

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -129,22 +129,27 @@ class ApiController extends Controller
 
         return $response;
     }
-    public function tstSelectAnswer(Request $request) {
-        $subQuery = DB::table('select_tile_results')
-            ->select('answer as hai', DB::raw("'true' as is_comment"))
-            ->where('display', '=', 1)
-            ->whereNotNull('comment');
 
-        $response = DB::table('select_tile_results')
-            ->select('answer as hai', DB::raw('count(answer) as count'), 'b.is_comment')
-            ->leftJoinSub(
-                $subQuery, 'b', function($join) {
-                $join->on('select_tile_results.answer', '=', 'b.hai');
-            })
-//            ->where('question_id', '=', $request['question']['id'])
-            ->groupBy('answer')
-            ->orderBy('count', 'desc')
-            ->get();
-        dd($response);
+    public function getSelectComment(Request $request, $id, $hai) {
+        try {
+            $response = DB::table('select_tile_results')
+                ->select('comment')
+                ->where('question_id', '=', $id)
+                ->where('answer', '=', $hai)
+                ->where('display', '=', 1)
+                ->whereNotNull('comment')
+                ->orderBy('updated_at', 'desc')
+                ->get()
+            ;
+
+
+        } catch (\Exception $e) {
+            Log::warning($e->getMessage());
+
+            $response = false;
+        }
+
+
+        return $response;
     }
 }
